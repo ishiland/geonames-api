98b7157ad09a75045a1a7d245ec02f8b8c4212c2
ishiland <ishiland@gmail.com>
2020-03-08 21:21:43 -0400

    cleanup

--- environment.ps1
+++
@@ -1,9 +0,0 @@
-# Environment setup for powershell users
-$env:POSTGRES_USER="postgres"
-$env:FLASK_ENV="development"
-$env:POSTGRES_PASSWORD="admin"
-$env:DATABASE_URL="postgresql://postgres:admin@localhost:5432/geonames"
-$env:DATABASE_URL_TEST="postgresql://postgres:admin@localhost:5432/geonames_test"
-$env:GEONAMES_DATA="allCountries"
-$env:APP_SETTINGS="config.DevelopmentConfig"
-$env:SECRET_KEY="houdini"
